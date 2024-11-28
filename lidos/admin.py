2f0dd7bab80061b3eccdce488f0973669d9bd8c9
Kate Eaves <3br90lcz@students.codeinstitute.net>
2024-11-28 13:00:46 +0000

    Add: Lido and Comment model to models.py in lidos app, Add: Test users and lido cards in admin panel

--- lidos/admin.py
+++ lidos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Lido
+from .models import Lido, Comment
 
 # Register your models here.
 admin.site.register(Lido)
+admin.site.register(Comment)
--- lidos/models.py
+++ lidos/models.py
@@ -10,12 +10,24 @@ class Lido(models.Model):
     slug = models.SlugField(max_length= 255, unique=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="lido_cards")
     lido_image = CloudinaryField('image', default='placeholder')
-    description = models.TextField()
-    highlight = models.TextField
+    description = models.TextField(null=True)
+    highlight = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     website = models.URLField(blank=True, null=True)
     
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
+
+class Comment(models.Model):
+    lido = models.ForeignKey(Lido, on_delete=models.CASCADE, related_name='comments')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    content = models.TextField()
+    is_approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user_id.username} on {self.lido.name}"
