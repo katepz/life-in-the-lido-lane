9a2efe910a27225cbaf0e588e204a5969761586a
Kate Eaves <3br90lcz@students.codeinstitute.net>
2024-11-29 07:07:26 +0000

    Create: script.js file and added to base.html, Add: lido_detail view

--- lidos/urls.py
+++ lidos/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.LidoList.as_view(), name = 'home'),
+    path('<slug:slug>/', views.lido_detail, name='lido_detail'),
 ]
--- lidos/views.py
+++ lidos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Lido
 
@@ -7,3 +7,23 @@ class LidoList(generic.ListView):
     queryset = Lido.objects.filter(status=1)
     template_name = "lidos/index.html"
     paginate_by = 6
+
+def lido_detail(request, slug):
+    """
+    Display an individual :model: `lidos.Lido`
+
+    **Context**
+    ``lido``
+        An instance of :model: `lidos.Lido`
+    
+    **Template**
+    :template:`lidos/lido_detail.html`
+    """
+    queryset = Lido.objects.filter(status=1)
+    lido = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "lidos/lido_detail.html",
+        {"lido": lido}
+    )
