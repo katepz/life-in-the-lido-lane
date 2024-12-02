b37b50d34acee63ffbc594547b54f859aee3f135
Kate Eaves <3br90lcz@students.codeinstitute.net>
2024-12-02 09:53:55 +0000

    Add: edit comment functionality

--- lidos/urls.py
+++ lidos/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.LidoList.as_view(), name = 'home'),
     path('<slug:slug>/', views.lido_detail, name='lido_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name = 'comment_edit'),
 ]
